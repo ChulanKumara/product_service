a0433202379ccdadaf03e9fff33a0b9d19f85b50
Oshada.E <sahani_95>
2021-01-06 18:00:28 +0530

    Docker file change

--- src/main/java/com/iit_ase_coursework/product_service/ProductServiceApplication.java
+++ src/main/java/com/iit_ase_coursework/product_service/ProductServiceApplication.java
@@ -1,13 +1,18 @@
 package com.iit_ase_coursework.product_service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductServiceApplication {
+public class ProductServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
